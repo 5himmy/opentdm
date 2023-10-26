3c5dde0175abbdb96fce9fc58dc5aab0a4d686f7
5himmy <572847+5himmy@users.noreply.github.com>
2023-10-26 20:09:59 +0300

    Update g_tdm_cmds.h

--- g_tdm_cmds.h
+++ g_tdm_cmds.h
@@ -1,12 +1,15 @@
 /*===============
 Commands Stuff
 ===============*/
+
 char *TDM_SettingsString (void);
 void TDM_Timeout_f (edict_t *ent);
 void TDM_PrintPlayers (edict_t *ent);
 void TDM_Motd_f (edict_t *ent);
 void TDM_OldScores_f (edict_t *ent);
-
+void TDM_CheckSafety(void);
+void TDM_ServerDemoRecord_f(edict_t *ent);
+void TDM_ServerDemoStop_f(edict_t *ent);
+void TDM_RandomMap_f(edict_t *ent);
 
 #define COMMAND_SPECLOCK		0x1
-
