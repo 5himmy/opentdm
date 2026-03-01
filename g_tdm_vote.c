7503ec860d161ce8f75b98ef4e55fa4b43d978ec
5himmy <okan_alkan@hotmail.com>
2026-03-02 00:53:14 +0300

    Fix undeclared 'forfeit' variable in vote restart

--- g_tdm_vote.c
+++ g_tdm_vote.c
@@ -238,7 +238,7 @@ void TDM_ApplyVote(void) {
         gi.bprintf(PRINT_CHAT, "Restarting the match...\n");
 
         //end the current match on the API before starting a new one
-        HTTP_PostMatchEndWithStats(&current_matchinfo, forfeit);
+        HTTP_PostMatchEndWithStats(&current_matchinfo, false);
 
         //ugly, but we need to free dynamic memory since the match start allocs a new array
         gi.TagFree(current_matchinfo.teamplayers);
